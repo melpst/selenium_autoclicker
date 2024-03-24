e63b6270f97d729828cd18b3bbcf24062057d3ae
melpst <jitlada.supreeyaporn@gmail.com>
2024-03-24 16:29:30 +0700

    👔 able to open chrome and click button

--- hello.py
+++ hello.py
@@ -1,1 +1,22 @@
-print("hello")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def main():
+    print("Start scraping")
+
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    time.sleep(1)
+
+    submit_button = driver.find_element(by = By.CSS_SELECTOR, value="button")
+    submit_button.click()
+    time.sleep(1)
+
+    driver.quit()
+
+    print("Quit successfully")
+
+main()
