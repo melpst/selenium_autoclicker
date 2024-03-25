19b1f0d718ac1a8580a3bff7381c2a0954fb9b77
melpst <jitlada.supreeyaporn@gmail.com>
2024-03-25 17:00:56 +0700

    👔 able to full screen and go to next episode

--- hello.py
+++ hello.py
@@ -4,9 +4,14 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from dotenv import load_dotenv
 
+from typing import List
 import os
 import time
 import re
+
+DELAY = 3
+INTRO = 35
+OUTRO = 30
 
 def main():
     print('Start scraping')
@@ -35,27 +40,66 @@ def main():
     password = os.getenv('PASSWORD')
 
     driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
 
     driver.get(url)
     login(driver, username, password)
     goto(driver, By.CLASS_NAME, 'recent-carousel')
     goto(driver, By.CLASS_NAME, 'season')
-    time.sleep(10)
+    (current_episode, left) = search_for_unwatched_episode(driver)
+    
+    for i in range(0, left):
+        print(f'currently playing episode {current_episode+i}')
+        time.sleep(DELAY)
+        toggle_full_screen(driver)
+        
+        interval = get_duration(driver)
+        print(interval)
+        time.sleep(interval)
+        toggle_full_screen(driver)
+        
+        next_button = driver.find_element(By.ID, 'solo-serieplay-ep-next')
+        if next_button is not None and i<left-1:
+            next_button.click()
+        else:
+            print('where is next button?')
+            break
 
+    print('watched all~~~')
+    time.sleep(DELAY)
     driver.quit()
 
     print('Quit successfully')
 
-def login(driver: WebDriver, username: str, password: str):
-    driver.find_element(by=By.ID, value="solo_login_email").send_keys(username)
-    driver.find_element(by=By.ID, value="solo_login_password").send_keys(password)
-    driver.find_element(by=By.CLASS_NAME, value="btn-login").click()
-    time.sleep(1)
+def login(driver: WebDriver, username: str, password: str) -> None:
+    driver.find_element(by=By.ID, value='solo_login_email').send_keys(username)
+    driver.find_element(by=By.ID, value='solo_login_password').send_keys(password)
+    driver.find_element(by=By.CLASS_NAME, value='btn-login').click()
+    time.sleep(DELAY)
 
-def goto(driver: WebDriver, find_by: str, value: str):
+def goto(driver: WebDriver, find_by: str, value: str) -> None:
     print('goto', value)
     element = driver.find_element(by=find_by, value=value)
-    element.find_element(by=By.CSS_SELECTOR, value="a").click()
-    time.sleep(1)
+    element.find_element(by=By.CSS_SELECTOR, value='a').click()
+    time.sleep(DELAY)
+
+def search_for_unwatched_episode(driver: WebDriver) -> List[int]:
+    elements = driver.find_elements(by=By.CLASS_NAME,value='progress-bar-danger')
+    not_played = list(filter(lambda x: int(re.findall(r'\d+', x.get_attribute('style'))[0]) < 95, elements))
+    not_played[0].find_element(by=By.XPATH, value='..').click()
+    time.sleep(DELAY)
+    return [len(elements)-len(not_played)+1,len(not_played)]
+    
+def toggle_full_screen(driver: WebDriver) -> None:
+    control_panel = driver.find_element(by=By.CLASS_NAME, value='media-control')
+    driver.execute_script("arguments[0].setAttribute('class', 'media-control')", control_panel)
+    elements = driver.find_elements(by=By.CLASS_NAME, value='media-control-button')
+    list(filter(lambda x: x.accessible_name == 'fullscreen', elements))[0].click()
+    time.sleep(DELAY)
+
+def get_duration(driver: WebDriver) -> int:
+    video = driver.find_element(by=By.CSS_SELECTOR, value='video')
+    duration = int(video.get_property('duration'))
+    return duration-(INTRO+OUTRO)
+
 main()
