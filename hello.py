c2a4db124533ac0865adc5795b43bd0414cb0336
melpst <jitlada.supreeyaporn@gmail.com>
2024-03-25 13:09:41 +0700

    👔 add login and click link

--- hello.py
+++ hello.py
@@ -34,15 +34,28 @@ def main():
     username = os.getenv('USERNAME')
     password = os.getenv('PASSWORD')
 
-
     driver = webdriver.Chrome()
     driver.implicitly_wait(5)
 
     driver.get(url)
+    login(driver, username, password)
+    goto(driver, By.CLASS_NAME, 'recent-carousel')
+    goto(driver, By.CLASS_NAME, 'season')
     time.sleep(10)
 
     driver.quit()
 
     print('Quit successfully')
 
+def login(driver: WebDriver, username: str, password: str):
+    driver.find_element(by=By.ID, value="solo_login_email").send_keys(username)
+    driver.find_element(by=By.ID, value="solo_login_password").send_keys(password)
+    driver.find_element(by=By.CLASS_NAME, value="btn-login").click()
+    time.sleep(1)
+
+def goto(driver: WebDriver, find_by: str, value: str):
+    print('goto', value)
+    element = driver.find_element(by=find_by, value=value)
+    element.find_element(by=By.CSS_SELECTOR, value="a").click()
+    time.sleep(1)
 main()
