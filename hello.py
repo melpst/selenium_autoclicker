b487359de02ec441d195b0749de10595174c14b0
melpst <jitlada.supreeyaporn@gmail.com>
2024-03-25 13:02:09 +0700

    💡 add what this code should be done

--- hello.py
+++ hello.py
@@ -1,22 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from dotenv import load_dotenv
+
+import os
 import time
+import re
 
 def main():
-    print("Start scraping")
+    print('Start scraping')
+    
+    """
+        1. open web
+        2. login
+        3. go to recently watch
+        4. click the first one
+        5. click bottom img
+        6. click latest watched+1 episode
+
+        loop from here
+        7. full screen
+        8. sleep until end
+        9. quit full screen
+        10. next episode
+
+        end loop if no more episode
+        quit
+    """
+
+    load_dotenv()
+    url = os.getenv('URL')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
 
 
     driver = webdriver.Chrome()
     driver.implicitly_wait(5)
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-    time.sleep(1)
 
-    submit_button = driver.find_element(by = By.CSS_SELECTOR, value="button")
-    submit_button.click()
-    time.sleep(1)
+    driver.get(url)
+    time.sleep(10)
 
     driver.quit()
 
-    print("Quit successfully")
+    print('Quit successfully')
 
 main()
