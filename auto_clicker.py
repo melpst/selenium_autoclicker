1be59e8fc080eb36cdf4c8edf4328d83617c62d0
melpst <jitlada.supreeyaporn@gmail.com>
2024-04-13 14:13:08 +0700

    ♻️ add type for variables, refactoring style

--- auto_clicker.py
+++ auto_clicker.py
@@ -3,8 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
-from typing import List
+from typing import List, Dict
 import os
 import time
 import re
@@ -20,25 +21,24 @@ EPISODE_TO_WATCH = -1
 load_dotenv(override=True)
 
 def create_driver() -> WebDriver:
-    browser_options = Options()
-    browser_options.add_argument("user-data-dir=selenium")
-
-    driver: WebDriver = webdriver.Chrome(options=browser_options)
+    driver: WebDriver = webdriver.Chrome(
+        options = Options('user-data-dir=selenium')
+    )
     driver.implicitly_wait(TIMEOUT)
     driver.set_page_load_timeout(TIMEOUT)
 
     return driver
 
 def add_cookies(driver: WebDriver) -> WebDriver:
-    base_cookie: dict[str, str] = {
+    base_cookie: Dict[str, str] = {
         'domain': os.getenv('DOMAIN'),
         'path': '/'
     }
-    lem: dict[str, str] = {
+    lem: Dict[str, str] = {
         'name': 'lem',
         'value': os.getenv('lem')
     }
-    session: dict[str, str] = {
+    session: Dict[str, str] = {
         'name': 'user_loggedsession',
         'value': os.getenv('session')
     }
@@ -87,45 +87,53 @@ def main():
 
 def goto(driver: WebDriver, find_by: str, value: str) -> None:
     print('goto', value)
-    element = driver.find_element(by=find_by, value=value)
+    element: WebElement = driver.find_element(by=find_by, value=value)
     element.find_element(by=By.CSS_SELECTOR, value='a').click()
+
     time.sleep(DELAY)
 
 def search_for_unwatched_episode(driver: WebDriver) -> List[int]:
-    elements = driver.find_elements(by=By.CLASS_NAME,value='progress-bar-danger')
-    not_played = list(filter(lambda x: int(re.findall(r'\d+', x.get_attribute('style'))[0]) < 90, elements))
+    elements: List[WebElement] = driver.find_elements(by=By.CLASS_NAME,value='progress-bar-danger')
+    not_played: List[WebElement] = list(
+        filter(
+            lambda x: int(re.findall(r'\d+', x.get_attribute('style'))[0]) < 90, elements
+        )
+    )
     not_played[0].find_element(by=By.XPATH, value='..').click()
+    
     time.sleep(DELAY)
     return [len(elements)-len(not_played)+1,len(not_played)]
     
 def toggle_full_screen(driver: WebDriver) -> None:
-    control_panel = driver.find_element(by=By.CLASS_NAME, value='media-control')
+    control_panel: WebElement = driver.find_element(by=By.CLASS_NAME, value='media-control')
     driver.execute_script("arguments[0].setAttribute('class', 'media-control')", control_panel)
     time.sleep(3)
 
-    elements = driver.find_elements(by=By.CLASS_NAME, value='media-control-button')
-    list(filter(lambda x: x.accessible_name == 'fullscreen', elements))[0].click()
+    elements: List[WebElement] = driver.find_elements(by=By.CLASS_NAME, value='media-control-button')
+    list(
+        filter(lambda x: x.accessible_name == 'fullscreen', elements)
+    )[0].click()
+    
     time.sleep(DELAY)
 
 def get_duration(driver: WebDriver) -> int:
-    video = driver.find_element(by=By.CSS_SELECTOR, value='video')
-    current_time = int(video.get_property('currentTime'))
-    duration = int(video.get_property('duration'))
+    video: WebElement = driver.find_element(by=By.CSS_SELECTOR, value='video')
+    current_time: int = int(video.get_property('currentTime'))
+    duration: int = int(video.get_property('duration'))
+
     return duration-current_time-OUTRO
 
 def skip_to_episode(driver: WebDriver, episode: int) -> List[int]:
-    episodes = driver.find_elements(by=By.CLASS_NAME, value='carousel-cell-left_subtitle')
+    episodes: List[WebElement] = driver.find_elements(by=By.CLASS_NAME, value='carousel-cell-left_subtitle')
     
     if len(episodes)<episode:
         return [-1, -1]
     
-    wanted_episode = episodes[episode-1]
-    left = len(episodes)-episode
-
     print(f'skip to episode {episode}')
-    wanted_episode.click()
+    episodes[episode-1].click()
+    
     time.sleep(DELAY)
-    return [episode, left]
+    return [episode, len(episodes)-episode]
 
 def loop(driver: WebDriver, current: int, left: int) -> None:
     for i in range(0, left):
@@ -133,12 +141,12 @@ def loop(driver: WebDriver, current: int, left: int) -> None:
         time.sleep(5)
         toggle_full_screen(driver)
         
-        interval = get_duration(driver)
+        interval: int = get_duration(driver)
         print(interval)
         time.sleep(interval)
         toggle_full_screen(driver)
         
-        next_button = driver.find_element(By.ID, 'solo-serieplay-ep-next')
+        next_button: WebElement = driver.find_element(By.ID, 'solo-serieplay-ep-next')
         if next_button is not None:
             next_button.click()
         else:
